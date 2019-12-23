410bc0a029844971b5a610cacea18404c239ad12
彭付林 <pfl(*)2016>
2019-12-23 16:02:20 +0800

    1.提交测试

--- app/src/main/java/com/peng/administrator/myobjectkotlin/MainActivity.kt
+++ app/src/main/java/com/peng/administrator/myobjectkotlin/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     //    private lateinit var viewPageAdapter: ViewPageAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         initView()
         Log.i("MainActivity", "---->onCreate")
